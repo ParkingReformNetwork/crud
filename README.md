164d5a6400aaa9efd81736f86594a6dfcfc62aa2
Alan Vuong <alankvuong@gmail.com>
2023-11-07 05:03:08 -0800

    Adding initial setup files for project. Set up prettier, eslint, parcel, and typesript. Added a temporary home screen that displays on project first load

---
+++ README.md
@@ -0,0 +1,45 @@
+# CRUD Database App
+
+This code runs the CRUD Database application for volunteers to interact with, input, and update datbase information relating to the Parking Lot Map and Parking Reform Map.
+
+This project utilizes React, Typescript, and Parcel.
+
+# Running the database app
+
+1. Install [Node Package Manager (npm)](https://nodejs.dev/en/download/).
+
+2. Run `npm i` in the main folder.
+
+### Start the development server
+
+```bash
+❯ npm run start
+```
+
+Then open http://localhost:1234 in a browser. Hit `CTRL-C` to stop the development server.
+
+When the server is running, you can make any changes you want to the project. Reload the page in the browser to see those changes. (You may need to force reload, e.g. hold the shift key while reloading on macOS.)
+
+### Autoformat Code Using Prettier
+To fix the format of your code before pushing it up to the repository, you can run the following command to do a check for you:
+
+```bash
+> npm run fmt
+```
+
+### Check For Consistency Errors in Code
+To check if there are any errors in your code, such as unused variables, use the following command:
+
+```bash
+> npm run fix
+```
+
+### Lint Code
+"Linting" means using tools that check for common issues that may be bugs or low code quality.
+
+```bash
+❯ npm run lint
+```
+
+# Tests
+There are currently no tests for this repository. Tests will be added as there is more development.
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import PRN_LOGO from "../../../public/imgs/PRN_logo.png";
+import "./home.scss";
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function HomePage() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          className="background-image-overlay"
+          sx={{
+            backgroundColor: "#80E4BC",
+            position: "relative",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box className="form-logo-box">
+            <Avatar
+              className="prn-form-logo"
+              alt="Parking Reform Network logo"
+              src={PRN_LOGO}
+            />
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
