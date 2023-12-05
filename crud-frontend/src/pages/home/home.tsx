8d02860f9dbcb461d324d6367ba79770e3e6a2c8
Alan Vuong <alankvuong@gmail.com>
2023-12-05 00:18:56 -0800

    removing unnecessary theme provider import from home.tsx

--- crud-frontend/src/pages/home/home.tsx
+++ crud-frontend/src/pages/home/home.tsx
@@ -5,7 +5,6 @@ import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import PRN_LOGO from "../../../public/imgs/PRN_logo.png";
 import "./home.scss";
 
