f01da2a20d08b93eaeedb2f1614420f8434fade0
Alan Vuong <alankvuong@gmail.com>
2024-03-05 09:30:36 -0800

    PR revisions

---
+++ crud/src/contexts/AuthContext.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { createContext, useState, useEffect, useContext, ReactNode } from "react";
+import { createClient, Session, AuthChangeEvent } from "@supabase/supabase-js";
+import { supabase } from "../setup/supabaseSetup"
+
+interface AuthContextProps {
+	session: Session | null;
+	user: any;
+}
+const AuthContext = createContext<AuthContextProps>({session: null, user: null});
+
+export const useAuth = () => useContext(AuthContext)
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({children}) => {
+	const [session, setSession] = useState<Session | null>(null);
+	const [user, setUser] = useState<any>(null);
+
+	useEffect(() => {
+		const sessionListener = supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
+			setSession(session);
+			setUser(session?.user ?? null);
+		});
+
+		const fetchSession = async() => {
+			try {
+				const { data, error } = await supabase.auth.getSession();
+
+				if(error) {
+					// Handle error if any
+					console.error("Error fetching session: ", error)
+				} else if (data && data.session) {
+					// session exists
+					setSession(data.session);
+					setUser(data.session.user ?? null);
+				} else {
+					// no session exists, set seesion state to null
+					setSession(null);
+					setUser(null);
+				}
+			} catch (error) {
+				console.error("Unexpected error fetching session: ", error)
+			}
+		};
+
+		fetchSession();
+
+		return () => {
+			if(sessionListener.data.subscription) {
+				sessionListener.data.subscription.unsubscribe();
+			}
+		};
+	}, []);
+
+	return (
+		<AuthContext.Provider value={{ session, user }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
